056d34c0ce0a1804ea5a6484b5c4618f8df0f565
Paul <22284856+ProgramComputer@users.noreply.github.com>
2020-05-14 18:10:38 -0500

    Add files via upload

---
+++ no500.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+echo "Please press ENTER to exit"
+
+PASS='admin'
+IP='192.168.1.1'
+ACTION='reboot.tri'
+ENTRY=''
+REGEX='([^0-9]|^)500([^0-9]|$)'
+
+while getopts ":i:p:a:e:r:u:" opt; do
+  case $opt in
+    i) i="$OPTARG"
+    ;;
+    p) p="$OPTARG"
+    ;;
+    a) a="$OPTARG"
+    ;;
+    e) e="$OPTARG"
+    ;;
+    r) r="$OPTARG"
+    ;;
+    u) u="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+#  if (( $OPTIND == 1 )); then
+#   $i = $IP
+#  $p = $PASS
+#   $a = $ACTION
+   
+   
+   
+   
+#fi
+done
+
+if [ -z "$i" ]
+then 
+true
+else
+IP = i
+fi
+
+if [ -z "$p" ]
+ then
+ true
+  
+else
+PASS=$p
+
+
+
+fi
+if [ -z "$a" ]
+ then
+ true
+else
+ACTION=$a
+fi
+if [ -z "$e" ]
+ then
+ true
+else
+ENTRY=$e
+fi
+if [ -z "$r" ]
+ then
+ true
+else
+REGEX=$r
+fi
+
+#if [ -z "$1" ] || [-z "$2" ]
+ # then
+    #echo "No argument supplied"
+#else
+
+
+while true;
+do
+RESPONSE=response.txt
+
+status="$(curl -s -w %{http_code} -u $u:${PASS} 192.168.1.1 -o $RESPONSE)"
+if [ $status = "401" ] || [ $status == '500' ]; 
+then
+ curl -f -u $u:$PASS -d "$ENTRY" $IP/$ACTION
+    break
+   fi
+
+  if [[ $(curl -f -u $u:${PASS} ${IP}) =~ $REGEX ]]; then
+    curl -f -u $u:$PASS -d "$ENTRY" $IP/$ACTION
+    break
+   fi
+    read -t 0.25 -N 1 input
+   if [[ $input = "q" ]] || [[ $input = "Q" ]]; then
+
+       echo
+       break 
+   fi
+done
+
+
+
